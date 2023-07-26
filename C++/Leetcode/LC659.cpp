caee6792dc07a9b835f9ed5007f43930c21f7bfb
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-07-26 11:11:04 -0600

    Initial Leetcode 659 (not started)

---
+++ C++/Leetcode/LC659.cpp
@@ -0,0 +1,22 @@
+// Taken from Lintcode
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    string encode(vector<string> &strs) {
+        // write your code here
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    vector<string> decode(string &str) {
+        // write your code here
+    }
+};
