a1d35eefc55dc566c8105670e26d5dcebe9dbf3b
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-08-15 14:32:09 -0600

    JS Variables and Basic Operations

---
+++ Odin Project/JS/intro.js
@@ -0,0 +1,40 @@
+// console.log("Hello, World!");
+let message = "Hello World!";
+// alert(message);
+
+let user = "John",
+  age = 25,
+  msg = "Hi"; // Less readable but convenient
+
+let user2 = "Bob",
+  age2 = 17,
+  message2 = "Hello John"; // More readable version
+
+const JOHN_BDAY = "18.04.1982";
+const HEX_ORANGE = "#FF7F00";
+
+// Task 1
+let admin;
+let name;
+
+name = "John";
+admin = name;
+// alert(admin);
+
+// Task 2
+let planetName = "Earth";
+let currUser;
+
+// TOP Assignment
+
+// 1-3
+console.log(23 + 97);
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+console.log((4 + 6 + 9) / 77);
+
+// 4-5
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
