570d0cf9cc4462052df1e7431110b8110fb59e09
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-08-15 14:30:27 -0600

    Fizzbuzz in JS, and fn practice

---
+++ Odin Project/JS/f3.js
@@ -0,0 +1,12 @@
+function add7(num) {
+  return num + 7;
+}
+let multiply = (a, b) => a * b;
+let capitalize = (s) => s.charAt(0).toUpperCase();
+let lastLetter = (s) => s.charAt(s.length - 1);
+
+console.log(add7(12));
+console.log(multiply(7, 9));
+console.log(capitalize("hi my name is Kevin"));
+console.log(capitalize("A"));
+console.log(lastLetter("Hello world!"));
